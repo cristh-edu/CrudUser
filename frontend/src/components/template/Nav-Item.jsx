ebadde01f12b95d3b6f853eeb1ba427c11d5140b
cristhedu <contato.cristhian.slopes@gmail.com>
2022-04-05 10:13:48 -0300

    Implementando Rotas

--- frontend/src/components/template/Nav-Item.jsx
+++ frontend/src/components/template/Nav-Item.jsx
@@ -1,7 +1,8 @@
 import './Nav-Item.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
-    <a href={`#/${props.link}`}>
+    <Link to={`/${props.link}`}>
         <i className={`fa fa-${props.icon}`}></i> {props.title}
-    </a>
+    </Link>
